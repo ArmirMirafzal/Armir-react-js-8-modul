1974dca91068493cf388d14ba99b801e275a5199
Armir <mirarmirkhan@gmail.com>
2023-06-18 19:13:09 +0500

    this branch start code

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <>
-      <h1>master</h1>
+      <h1>89-dars</h1>
     </>
   </React.StrictMode>
 );
