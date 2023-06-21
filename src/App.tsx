269ab743a1dd2aaf4b56788891db5b848797e80b
Armir <mirarmirkhan@gmail.com>
2023-06-21 11:38:04 +0500

    89-dars uy ishisini 90-darsda yana takroran berdila

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import Box from './components/box'
+
+
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <Box />
+      </div>
+    )
+  }
+}
---
+++ src/components/box.tsx
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+interface BoxProps {}
+
+export default class Box extends Component<BoxProps, {}> {
+	render() {
+		return <div>hello world</div>;
+	}
+}
